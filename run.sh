30801816363709c92e856eb42ee1be548c3a7049
Dror Matalon <dror@matal.com>
2024-11-22 10:39:52 -0800

    Add API key export and update script symlinks and execution command

--- run.sh
+++ run.sh
@@ -5,15 +5,17 @@ PIPELINES_URLS=''
 REQUIREMENTS_FILE=''
 PIPELINES_REQUIREMENTS_PATH=''
 export ANTHROPIC_API_KEY=`cat ~/.anthropic/api_key`
+export WEATHER_API_KEY=`cat ~/.weather`
 
 
 cd pipelines
+rm -f failed/*
 rm -f *.py
-ln -s ../dev/*.py .
+ln -s ../dev/weather.py .
+ln -s ../dev/compute.py .
 cd ..
 
 # Default to development mode with auto-reload enabled
 # Can be overridden by setting UVICORN_EXTRA_FLAGS before running
-export UVICORN_EXTRA_FLAGS="--reload"
 
-./start.sh
+./dev.sh
